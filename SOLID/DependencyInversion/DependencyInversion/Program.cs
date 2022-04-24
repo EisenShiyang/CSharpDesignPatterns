e4fe5ba55420713e9eeecddb4cdcd127d15237c0
Shiyang <39637745+EisenShiyang@users.noreply.github.com>
2022-04-24 17:04:27 +0800

    Uploaded Liskov and dependency Inversion
    Uploaded Liskov and dependency Inversion

---
+++ SOLID/DependencyInversion/DependencyInversion/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DependencyInversion
+{
+    class Program
+    {
+        public class Research 
+        {
+            //public Research(Relationships relationships)
+            //{
+            //    var relations = relationships.Relations;
+            //    foreach(var v in relations.Where(x => x.Item1.Name=="Hank" && x.Item2 == Relationship.Parent))
+            //    {
+            //        Console.WriteLine("Hank has a child called "+v.Item3.Name);
+            //    }
+            //}
+            public Research(IRelationshipBrowser b)
+            {
+                foreach (var v in b.FindAllChildrenOf("Hank"))
+                    Console.WriteLine("Hank has a child called " + v.Name);
+            }
+            static void Main(string[] args)
+            {
+                var parent = new Person { Name = "Hank" };
+                var child1 = new Person { Name = "Candy" };
+                var child2 = new Person { Name = "Candy2" };
+                var relationships = new Relationships();
+                relationships.AddParentAndChild(parent, child1);
+                relationships.AddParentAndChild(parent, child2);
+                new Research(relationships);
+            }
+        }
+        public enum Relationship
+        {
+            Parent,
+            Child,
+            Sibling
+        }
+        public class Person
+        {
+            public string Name;
+        }
+        public interface IRelationshipBrowser
+        {
+            IEnumerable<Person> FindAllChildrenOf(string name);
+        }
+        public class Relationships : IRelationshipBrowser
+        {
+            private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+            public void AddParentAndChild(Person parent, Person child)
+            {
+                relations.Add((parent, Relationship.Parent, child));
+                relations.Add((child, Relationship.Child, parent));
+            }
+
+            public IEnumerable<Person> FindAllChildrenOf(string name)
+            {
+                return relations.Where(x => x.Item1.Name==name && x.Item2 == Relationship.Parent).Select(r => r.Item3);
+            }
+
+            public List<(Person, Relationship, Person)> Relations => relations;
+
+        }
+    }
+}
---
+++ SOLID/Liskov/Liskov/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Liskov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(3, 4);
+            Console.WriteLine(Area(r));
+            Rectangle s = new Square();
+            s.width = 4;
+            Console.WriteLine(Area(s));
+        }
+        public static int Area(Rectangle r)
+        {
+            return r.width * r.height;
+        }
+
+        public class Rectangle
+        {
+            public virtual int width { get; set; }
+            public virtual int height { get; set; }
+            public Rectangle(int w, int h)
+            {
+                width = w;
+                height = h;
+            }
+            public Rectangle()
+            {
+            }
+        }
+
+        public class Square : Rectangle
+        {
+            public override int width
+            {
+                set
+                {
+                    base.width = base.height = value;
+                }
+            }
+            public override int height
+            {
+                set
+                {
+                    base.height = base.width = value;
+                }
+            }
+        }
+    }
+}
