f7f82dc931190d7832d61cff656165d8b415aad8
Shiyang <39637745+EisenShiyang@users.noreply.github.com>
2022-06-06 21:49:01 +0800

    Upload LifeWithoutBuilder

---
+++ Builder/LifeWithoutBuilder/LifeWithoutBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace LifeWithoutBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb.ToString());
+
+            sb.Clear();
+            var words = new[] { "hello", "world" };
+
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li> {0} </li>", word);
+            }
+            sb.Append("</ul>");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
